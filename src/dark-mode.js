8972894f46d0a7458c97528e9f8da16b3e64b235
kgdn <kjg2000@hw.ac.uk>
2022-06-13 16:03:43 +0100

    I think I suck at JavaScript

--- src/dark-mode.js
+++ src/dark-mode.js
@@ -1,5 +1,5 @@
 // create dark mode toggle and store in local storage
-const darkModeButton = document.getElementsByClassName("dark-mode-button");
+const darkModeButton = document.getElementById("dark-mode-button");
 
 darkModeButton.addEventListener("click", () => {
   const darkMode = localStorage.getItem("darkMode");
@@ -8,7 +8,7 @@ darkModeButton.addEventListener("click", () => {
     localStorage.setItem("darkMode", "disabled");
     document.body.classList.remove("dark-mode");
   } else {
-    darkModeButton.innerHTML = "🌞";
+    darkModeButton.innerHTML = "☀️";
     localStorage.setItem("darkMode", "enabled");
     document.body.classList.add("dark-mode");
   }
