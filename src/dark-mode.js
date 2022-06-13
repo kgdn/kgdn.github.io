f1f98e4db4454205f1517e2f57dbaf737a6c82d1
kgdn <kjg2000@hw.ac.uk>
2022-06-13 16:10:51 +0100

    another one......

--- src/dark-mode.js
+++ src/dark-mode.js
@@ -1,15 +1,27 @@
 // create dark mode toggle and store in local storage
 const darkModeButton = document.getElementById("dark-mode-button");
+let darkMode = localStorage.getItem("darkMode");
+
+const enableDarkMode = () => {
+    body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+}
+
+const disableDarkMode = () => {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", null);
+}
+
+if (darkMode === "enabled") {
+    enableDarkMode();
+}
 
 darkModeButton.addEventListener("click", () => {
-  const darkMode = localStorage.getItem("darkMode");
-  if (darkMode === "enabled") {
-    darkModeButton.innerHTML = "🌙";
-    localStorage.setItem("darkMode", "disabled");
-    document.body.classList.remove("dark-mode");
-  } else {
-    darkModeButton.innerHTML = "☀️";
-    localStorage.setItem("darkMode", "enabled");
-    document.body.classList.add("dark-mode");
-  }
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        enableDarkMode();
+    }
+    else {
+        disableDarkMode();
+    }
 });
