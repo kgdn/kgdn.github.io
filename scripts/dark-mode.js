46de1b8308d1f16056a8f63616d6c47c37935ffe
kgdn <kjg2000@hw.ac.uk>
2022-06-13 15:35:03 +0100

    Local storage attempt

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,4 +1,10 @@
 function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  if (element.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "true");
+  }
+  else {
+    localStorage.setItem("dark-mode", "false");
+  }
 }
