ba552af5de332f022941c834fffe689d3364a7dd
kgdn <kjg2000@hw.ac.uk>
2022-06-13 22:43:15 +0100

    Remove thing

--- src/theme.js
+++ src/theme.js
@@ -3,19 +3,6 @@ const body = document.body;
 
 const theme = localStorage.getItem('theme');
 
-/* Taken from https://github.com/utterance/utterances/issues/549 */
-function utterancesTheme () {
-    if (document.querySelector('.utterances-frame')) {
-      const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'github-dark' : 'github-light'
-      const message = {
-        type: 'set-theme',
-        theme: theme
-      };
-      const iframe = document.querySelector('.utterances-frame');
-      iframe.contentWindow.postMessage(message, 'https://utteranc.es');
-    }
-  }
-
 if (theme) {
     body.classList.add(theme);
 }
@@ -24,11 +11,9 @@ themeSwitcher.onclick = () => {
     if (body.classList.contains('dark')) {
         body.classList.replace('dark', 'light');
         localStorage.setItem('theme', 'light');
-        utterancesTheme();
     } else {
         body.classList.replace('light', 'dark');
         localStorage.setItem('theme', 'dark');
-        utterancesTheme();
     }
 };
 
