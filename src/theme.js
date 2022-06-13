f07b1080a8444617b27f1454497bc6cf92ac3720
kgdn <kjg2000@hw.ac.uk>
2022-06-13 22:38:03 +0100

    Utterances dark mode test

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,19 @@ const body = document.body;
 
 const theme = localStorage.getItem('theme');
 
+/* Taken from https://github.com/utterance/utterances/issues/549 */
+function utterancesTheme () {
+    if (document.querySelector('.utterances-frame')) {
+      const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'github-dark' : 'github-light'
+      const message = {
+        type: 'set-theme',
+        theme: theme
+      };
+      const iframe = document.querySelector('.utterances-frame');
+      iframe.contentWindow.postMessage(message, 'https://utteranc.es');
+    }
+  }
+
 if (theme) {
     body.classList.add(theme);
 }
@@ -11,9 +24,11 @@ themeSwitcher.onclick = () => {
     if (body.classList.contains('dark')) {
         body.classList.replace('dark', 'light');
         localStorage.setItem('theme', 'light');
+        utterancesTheme();
     } else {
         body.classList.replace('light', 'dark');
         localStorage.setItem('theme', 'dark');
+        utterancesTheme();
     }
 };
 
